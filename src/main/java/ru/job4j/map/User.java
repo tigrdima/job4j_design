da830c11280fb2d2148ad0f6b883b084f8c7306f
tigrdima <duys79yandex.ru>
2022-03-10 13:12:38 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -2,14 +2,18 @@ package ru.job4j.map;
 
 import java.util.*;
 
+import static java.util.Calendar.*;
+import static java.util.Objects.hash;
+
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
 
-    public User(String name, int children) {
+    public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
+        this.birthday = birthday;
     }
 
     @Override
@@ -17,17 +21,29 @@ public class User {
         return "User{"
                 + "name='" + name + '\''
                 + ", children=" + children
-                + ", birthday=" + birthday
                 + '}';
     }
 
+    @Override
+    public int hashCode() {
+        return hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
-        User user1 = new User("Dima", 2);
-        User user2 = new User("Dima", 2);
+        GregorianCalendar birthdayUser1 = new GregorianCalendar(1979, JANUARY, 9, 0, 0, MILLISECOND);
+        GregorianCalendar birthdayUser2 = new GregorianCalendar(1979, JANUARY, 9, 0, 0, MILLISECOND);
+
+        User user1 = new User("Dima", 2, birthdayUser1);
+        User user2 = new User("Dima", 2, birthdayUser2);
+
         Map<User, Object> map = new HashMap<>();
+
         map.put(user1, new Object());
         map.put(user2, new Object());
 
-            System.out.println(map);
-        }
+        System.out.println(user1.hashCode() + " " + user2.hashCode());
+        System.out.println(map);
+
+    }
 }
+
