b2689957d90a41175812cdbb7a709d65d8cc6910
tigrdima <duys79yandex.ru>
2022-03-10 13:14:04 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -25,6 +25,11 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
     public int hashCode() {
         return hash(name, children, birthday);
     }
