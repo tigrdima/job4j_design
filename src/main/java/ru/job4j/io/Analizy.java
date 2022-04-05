4a1d70fc6d4f2ccef0982ed44c95ff1d23dd5cbe
tigrdima <duys79yandex.ru>
2022-04-05 11:21:24 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Analizy {
+    private String start;
+    private String end;
+    private boolean count;
+    private boolean rsl;
+
+    public void unavailable(String source, String target) {
+
+        try (FileReader reader = new FileReader(source);
+             BufferedReader in = new BufferedReader(reader);
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+
+            Pattern pattern = Pattern.compile("^500|^400");
+
+            in.lines().forEach(line -> {
+                Matcher matcher = pattern.matcher(line);
+                rsl = matcher.find();
+                String[] s = line.split(" ");
+
+                if (rsl && !count) {
+                    start = s[1];
+                    count = true;
+                    return;
+                } else if (rsl || !count) {
+                    return;
+                }
+                count = false;
+                end = s[1];
+                out.printf("%s%s%s%n", start, " ; ", end);
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("logServer.csv", "unavailable.csv");
+    }
+}
