6d895ef09766ba80e522db03a4d7f8fc20bd89ba
tigrdima <duys79yandex.ru>
2022-04-06 13:17:14 +0300

    4.1. Сканирование файловой системы. [#106929 #285165]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -11,6 +11,7 @@ import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
+
     private Predicate<Path> condition;
     private List<Path> files = new ArrayList<>();
 
