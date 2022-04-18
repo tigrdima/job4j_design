7dde46712c877e7035b1e469441aadcdb1bea330
tigrdima <duys79yandex.ru>
2022-04-18 13:34:17 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,7 @@
 package ru.job4j.io;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +10,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    static final String HELLO = "^.+\\?msg=Hello\\s.+$";
+    static final String EXIT = "^.+\\?msg=Exit\\s.+$";
+    static final String WHAT = "^.+\\?msg=What\\s.+$";
+    static final String ANSWER = "HTTP/1.1 200 OK\r\n\r\n";
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -16,13 +23,22 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
 
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("?msg=Bye")) {
-                            server.close();
-                        }
-                        System.out.println(str);
+                    String str = in.readLine();
+
+                    if (str.matches(HELLO)) {
+                        out.write(ANSWER.getBytes());
+                        out.write("Hello".getBytes());
+                    } else if (str.matches(EXIT)) {
+                        out.write(ANSWER.getBytes());
+                        out.write("Server shut down".getBytes());
+                        server.close();
+                    } else if (str.matches(WHAT)) {
+                        out.write(ANSWER.getBytes());
+                        out.write("What".getBytes());
+                    } else {
+                        out.write(ANSWER.getBytes());
+                        out.write(str.getBytes());
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.flush();
                 }
             }
