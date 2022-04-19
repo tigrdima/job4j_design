cf0644343311cff62319dc2c6ea4d520477f6e38
tigrdima <duys79yandex.ru>
2022-04-19 13:40:26 +0300

    2. Формат JSON [#313164 #290901]

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import java.io.Serializable;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -16,9 +17,22 @@ public class Main {
         final Car car = new Car(new RegNumberCar("678FGH"), "Audi", new String[]{"Abs", "A/C", "Leather interior"},
                 true, 2000);
         final String carJson = JSONCAR.toJson(car);
-        System.out.println(carJson);
-        final Car carMod = JSONCAR.fromJson(carJson, Car.class);
-        System.out.println(carMod);
+        System.out.println("Serialization : " + carJson);
+
+        final String fromStringToObject =
+                "{"
+                        + "\"regNumberCar\": {"
+                        + "\"number\":\"085FYW\""
+                        + "},"
+                        + "\"name\":\"Lada\","
+                        + "\"options\":"
+                        + "[\"nonAbs\",\"nonA/C\",\"Fabric interior\"],"
+                        + "\"automaticTransmission\": false,"
+                        + "\"carMileage\":23450"
+                        + "}";
+
+        final Car carMod = JSONCAR.fromJson(fromStringToObject, Car.class);
+        System.out.println("Deserialization : " + carMod + System.lineSeparator());
 
         JAXBContext context = JAXBContext.newInstance(Car.class);
         Marshaller marshaller = context.createMarshaller();
