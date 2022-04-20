aaf10aa43bccdc4e4a1d02b64daf3fa76e02aa21
tigrdima <duys79yandex.ru>
2022-04-20 09:30:59 +0300

    0. Что такое база данных. [#6860]

---
+++ script.sql
@@ -0,0 +1,11 @@
+create table students(
+ 	id serial primary key,
+	name varchar(255),
+	course phone,
+	int text
+);
+insert into students(name, course, phone) values ('Иван Иванов Иванович', 3, '+7 999 999 99 99');
+select * from students;
+update students set name = 'Иван Иванович';
+select * from students;
+delete from students;
