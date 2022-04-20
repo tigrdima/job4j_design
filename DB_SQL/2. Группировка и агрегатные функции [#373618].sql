c4ae9059417dc403610b7373d69a338f4c3e5c60
tigrdima <duys79yandex.ru>
2022-04-20 18:15:52 +0300

    2. Группировка и агрегатные функции [#373618 #291514]

--- DB_SQL/2. Группировка и агрегатные функции [#373618].sql
+++ DB_SQL/2. Группировка и агрегатные функции [#373618].sql
@@ -40,9 +40,9 @@ select avg(price) from devices;
 select p.name, avg(d.price)
 from devices_people as dp
 join people p
-on dp.device_id = p.id
+on dp.people_id = p.id
 join devices d
-on dp.people_id = d.id
+on dp.devices_id = d.id
 group by p.name
 having avg(d.price) > 5000
 
