82e5d11238ceacafe66c84c9541707e96fd53fe0
tigrdima <duys79yandex.ru>
2022-04-20 13:35:00 +0300

    0. Select [#6861 #291404]

--- DB_SQL/0. Select [#6861].sql
+++ DB_SQL/0. Select [#6861].sql
@@ -1,7 +1,7 @@
 select * from fauna where name like '%fish%';
 
-select * from fauna where avg_age>10000 and avg_age<21000;
+select * from fauna where avg_age between 10000 and 20000;
 
 select * from fauna where discovery_date isnull;
 
-select * from fauna where discovery_date > '1950';
+select * from fauna where discovery_date < '01/01/1950';
