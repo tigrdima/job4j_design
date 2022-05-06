bef1390b0324efd9f2ea79394b78dbc5e3ac3ce1
tigrdima <duys79yandex.ru>
2022-05-06 12:08:07 +0300

    1. Отчеты. [#850 #297225]

--- src/test/java/ru/job4j/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/srp/ReportEngineTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.srp;
 
 import org.junit.Test;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -34,23 +35,24 @@ public class ReportEngineTest {
     public void whenReportProgrammers() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 10000.45);
-        Employee worker2 = new Employee("Vova", now, now, 9056.40);
-        Employee worker3 = new Employee("Kolya", now, now, 20400.20);
+        Employee worker = new Employee("Ivan", now, now, 10000.5);
+        Employee worker2 = new Employee("Vova", now, now, 9056.4);
         store.add(worker);
         store.add(worker2);
-        store.add(worker3);
         Report engine = new ReportProgrammers(store);
         String ln = System.lineSeparator();
+
         StringBuilder expect = new StringBuilder();
         expect.append("<html>").append(ln).append("<body>").append(ln);
         expect.append("<p>").append("Name; Hired; Fired; Salary").append("</p>").append(ln);
-        for (Employee em : store.findBy(em -> true)) {
-            expect.append("<p>").append(em.getName()).append(";")
-                    .append(em.getHired()).append(";")
-                    .append(em.getFired()).append(";")
-                    .append(em.getSalary()).append(";").append("</p>").append(ln);
-        }
+        expect.append("<p>").append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary()).append(";").append("</p>").append(ln);
+        expect.append("<p>").append(worker2.getName()).append(";")
+                .append(worker2.getHired()).append(";")
+                .append(worker2.getFired()).append(";")
+                .append(worker2.getSalary()).append(";").append("</p>").append(ln);
         expect.append("</body>").append(ln).append("</html>");
 
         assertThat(engine.generate(em -> true), is(expect.toString()));
@@ -62,22 +64,25 @@ public class ReportEngineTest {
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 10000.45);
         Employee worker2 = new Employee("Vova", now, now, 9056.40);
-        Employee worker3 = new Employee("Kolya", now, now, 20400.20);
         store.add(worker);
         store.add(worker2);
-        store.add(worker3);
 
         Report engine = new ReportAccounting(store);
         String ln = System.lineSeparator();
+
         StringBuilder expect = new StringBuilder();
         expect.append("Name; Hired; Fired; Salary(EUR);").append(ln);
-        for (Employee em : store.findBy(em -> true)) {
-            expect.append(em.getName()).append(";")
-                    .append(em.getHired()).append(";")
-                    .append(em.getFired()).append(";")
-                    .append((int) (em.getSalary() / ReportAccounting.RATE)).append(";")
-                    .append(ln);
-        }
+        expect.append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append((int) (worker.getSalary() / ReportAccounting.RATE)).append(";")
+                .append(ln);
+        expect.append(worker2.getName()).append(";")
+                .append(worker2.getHired()).append(";")
+                .append(worker2.getFired()).append(";")
+                .append((int) (worker2.getSalary() / ReportAccounting.RATE)).append(";")
+                .append(ln);
+
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 
@@ -85,23 +90,28 @@ public class ReportEngineTest {
     public void whenReportHr() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
+
         Employee worker = new Employee("Ivan", now, now, 10000.45);
         Employee worker2 = new Employee("Vova", now, now, 9056.40);
         Employee worker3 = new Employee("Kolya", now, now, 20400.20);
-        store.add(worker);
-        store.add(worker2);
-        store.add(worker3);
+            store.add(worker);
+            store.add(worker2);
+            store.add(worker3);
         Report engine = new ReportHr(store);
-        List<Employee> employees = store.findBy(em -> true);
-        employees.sort((o1, o2) -> Double.compare(o2.getSalary(), o1.getSalary()));
         String ln = System.lineSeparator();
+
         StringBuilder expect = new StringBuilder();
         expect.append("Name; Salary;").append(ln);
-        for (Employee em : employees) {
-            expect.append(em.getName()).append(";")
-                    .append(em.getSalary()).append(";")
-                    .append(ln);
-        }
+        expect.append(worker3.getName()).append(";")
+                .append(worker3.getSalary()).append(";")
+                .append(ln);
+        expect.append(worker.getName()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(ln);
+        expect.append(worker2.getName()).append(";")
+                .append(worker2.getSalary()).append(";")
+                .append(ln);
+
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 
